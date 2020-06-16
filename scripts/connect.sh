5cbe74581e8516ef94c54316164101f1ba06aa14
Joshua Schmitt <joshua@pixelcubed.com>
2020-06-16 18:32:52 -0500

    fix ssh socket

--- scripts/connect.sh
+++ scripts/connect.sh
@@ -3,7 +3,7 @@ set -Eeuo pipefail
 
 MASTER_PORT=${MASTER_PORT:-22}
 
-until ssh -N -T -i /data/ssh/key -o UserKnownHostsFile=/data/ssh/known_hosts -p $MASTER_PORT -R /data/ospd.sock:/sockets/$HOSTNAME.sock gvm@$MASTER_ADDRESS; do
+until ssh -N -T -i /data/ssh/key -o UserKnownHostsFile=/data/ssh/known_hosts -p $MASTER_PORT -R /sockets/$HOSTNAME.sock:/data/ospd.sock gvm@$MASTER_ADDRESS; do
 	echo "Connection disrupted, retrying in 10 seconds..." >> /usr/local/var/log/gvm/ssh-connection.log
 	sleep 10
 done
